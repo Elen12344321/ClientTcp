218ae14829a39897cc5dc46ae923151843fd740f
elen12344321 <obublivska@gmail.com>
2019-04-26 23:06:24 +0300

    rmi client

---
+++ src/main/java/client/ConnectionHendlerRmi.java
@@ -0,0 +1,11 @@
+package client;
+
+import java.io.Closeable;
+import java.io.IOException;
+
+public class ConnectionHendlerRmi implements Closeable{
+    @Override
+    public void close() throws IOException {
+
+    }
+}
